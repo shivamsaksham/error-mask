5de9cc963cf84aa2902fe725325486917fae582c
devil-shiva <shivamsaksham2000@gmail.com>
2023-06-07 07:50:35 +0530

    made error not found fallback

--- src/index.js
+++ src/index.js
@@ -2,19 +2,50 @@ import { mongoDbErrors } from "./mongo_db";
 import { fireBaseAuthErr, firebaseFirestoreError, firebaseStorageError } from "./firebase_error"
 
 const getFirebaseAuthError = (error_code)=>{
-    return fireBaseAuthErr[error_code];
+    const msg = fireBaseAuthErr[error_code];
+    if (msg) {
+        return msg
+    }else{
+        fixNotFound(error_code)
+    }
 }
 
 const getFirebaseStoreError = (error_code)=>{
-    return firebaseFirestoreError[error_code];
+    const msg = firebaseFirestoreError[error_code];
+    if (msg) {
+        return msg
+    }else{
+        fixNotFound(error_code)
+    }
 }
 
 const getFirebaseStorageError = (error_code)=>{
-    return firebaseStorageError[error_code]
+    const msg = firebaseStorageError[error_code]
+    if (msg) {
+        return msg;
+    }else{
+        fixNotFound(error_code)
+    }
 }
 
 const getMongoError = (error_code)=>{
-    return mongoDbErrors[error_code]
+    const msg =  mongoDbErrors[error_code]
+    if (msg) {
+        return msg
+    }else{
+        fixNotFound(error_code)
+    }
+}
+
+const fixNotFound = (error_code)=>{
+    try {
+        let err = error_code.split("/")[1]
+        err = err.replace("-" , " ")
+        err = err.toUpperCase()
+        return err
+    } catch (error) {
+        return error_code
+    }
 }
 
 export {getFirebaseAuthError , getFirebaseStoreError , getFirebaseStorageError , getMongoError}
